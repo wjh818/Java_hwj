643d0c4e88d14694226facc7e2e5561e45e17717
wjh818@naver.com <wjh818@naver.com>
2020-12-03 16:03:18 +0900

    정렬: 보조 정렬

---
+++ myhadoop/src/main/java/myhadoop/support/GroupKeyComparator.java
@@ -0,0 +1,24 @@
+package myhadoop.support;
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class GroupKeyComparator extends WritableComparator {
+	// 셍성자
+	protected GroupKeyComparator() {
+		super(DateKey.class, true);
+	}
+
+	@Override
+	public int compare(WritableComparable a, WritableComparable b) {
+		// 형 변환
+		DateKey k1 = (DateKey)a;
+		DateKey k2 = (DateKey)b;
+		
+		// 파티션만 할 것이라서 년도 정보의 선후관계만 비교하면 된다
+		return k1.getYear().compareTo(k2.getYear());
+	}
+	
+	
+	
+}
